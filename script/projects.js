1395c452412c6463e8d186316f4270381e93eb02
Moses-Onche <onchemoses38@gmail.com>
2025-02-15 16:34:12 +0100

    Make page responsive

--- script/projects.js
+++ script/projects.js
@@ -1,17 +1,12 @@
 let hiddenContent = document.getElementsByClassName("hidden-content")[0];
 let readMore = document.getElementsByClassName("read-more")[0];
 
-console.log(hiddenContent);
-console.log(readMore);
-
 readMore.addEventListener("click", () => {
-    // for (let i = 0; i < hiddenContent.length; i++){
-        if (hiddenContent.style.display == "none") {
-            readMore.textContent = " Hide...";
-            hiddenContent.style.display = "block";
-        } else {
-            readMore.textContent = " Read more...";
-            hiddenContent.style.display = "none";
-        }
-    // }
+    if (hiddenContent.style.display == "block") {
+        hiddenContent.style.display = "none";
+        readMore.textContent = " Read more...";
+    } else {
+        hiddenContent.style.display = "block";
+        readMore.textContent = " Hide...";
+    }
 })
