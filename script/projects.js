feae08b367330f32b3bfaadb92ee318054c61a54
Moses-Onche <onchemoses38@gmail.com>
2025-03-01 17:48:37 +0100

    Build and style hamburger menu for both full screen mode and responsive mode

--- script/projects.js
+++ script/projects.js
@@ -1,6 +1,9 @@
-let hiddenContent = document.getElementsByClassName("hidden-content")[0];
-let readMore = document.getElementsByClassName("read-more")[0];
-let CV = document.getElementById("cv-link")
+const hiddenContent = document.getElementsByClassName("hidden-content")[0];
+const readMore = document.getElementsByClassName("read-more")[0];
+const CV = document.getElementById("cv-link");
+const hamLogo = document.getElementsByClassName("ham")[0].children[0];
+const hamMenu = document.getElementsByClassName("ham-menu")[0];
+console.log(hamMenu)
 
 readMore.addEventListener("click", () => {
     if (hiddenContent.style.display == "block") {
@@ -12,3 +15,15 @@ readMore.addEventListener("click", () => {
     }
 })
 
+hamLogo.addEventListener("click", () => {
+    if (hamMenu.style.display === "flex") {
+        hamMenu.style.display = "none";
+    } else {
+        hamMenu.style.position = "absolute";
+        // hamMenu.style.backgroundColor = "#e9f8f9";
+        hamMenu.style.top = "80px";
+        hamMenu.style.display = "flex";
+        hamMenu.style.flexDirection = "column";
+        hamMenu.style.justifyContent = "center";
+    }
+})
